e990e969f211214973c4ac001aac9d8fa9eab296
MelissaViana97 <70685997+MelissaViana97@users.noreply.github.com>
2023-03-25 18:01:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_app_sesion_calculadorainventario_dms6/activity_frame_layout.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_app_sesion_calculadorainventario_dms6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_frame_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
